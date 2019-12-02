1ea29c64ccc5fa16051bd22b3a30b2fac062702a
David Mehren <dmehren1@gmail.com>
2019-12-02 18:28:38 +0100

    Add tslint

--- day1/one.ts
+++ day1/one.ts
@@ -1,17 +1,17 @@
-import * as readline from "readline";
 import * as fs from "fs";
+import * as readline from "readline";
 
 const lineReader = readline.createInterface({
-    input: fs.createReadStream('input1.txt')
+    input: fs.createReadStream("input1.txt"),
 });
 
 let sum = 0;
 
-lineReader.on('line', (mass: number) => {
-    let fuel = Math.floor(mass / 3) - 2;
+lineReader.on("line", (mass: number) => {
+    const fuel = Math.floor(mass / 3) - 2;
     sum = sum + fuel;
 });
 
-lineReader.on('close', () => {
+lineReader.on("close", () => {
     console.log("Sum: ", sum);
 });
--- day1/two.ts
+++ day1/two.ts
@@ -1,27 +1,24 @@
-import * as readline from "readline";
 import * as fs from "fs";
+import * as readline from "readline";
 
 const lineReader = readline.createInterface({
-    input: fs.createReadStream('input1.txt')
+    input: fs.createReadStream("input1.txt"),
 });
 
 let sum = 0;
 
 function calculateFuel(mass: number): number {
-    let fuel = Math.floor(mass / 3) - 2;
-    if (fuel < 0) {
-        return 0;
-    } else {
-        return fuel + calculateFuel(fuel);
-    }
+    const fuel = Math.floor(mass / 3) - 2;
+    if (fuel < 0) { return 0; }
+    return fuel + calculateFuel(fuel);
 
 }
 
-lineReader.on('line', (mass: number) => {
-    let fuel = calculateFuel(mass);
+lineReader.on("line", (mass: number) => {
+    const fuel = calculateFuel(mass);
     sum = sum + fuel;
 });
 
-lineReader.on('close', () => {
+lineReader.on("close", () => {
     console.log("Sum: ", sum);
 });
